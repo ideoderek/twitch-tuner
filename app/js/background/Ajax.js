9766ad2571a5f6bb2660ca90d48e99f702256e09
ideoderek <ideoderek@gmail.com>
2017-10-11 08:21:15 -0230

    Refactors Ajax.js to ES2015

--- app/js/background/Ajax.js
+++ app/js/background/Ajax.js
@@ -1,95 +1,91 @@
-var Ajax = (function() {
-	function AjaxRequest(url) {
-		if (typeof url === 'string') {
-			this.url = url;
-		}
+function errorHandler() {
+	if (this.retries === 0) {
+		this.failure();
+	}
+	else {
+		this.retries -= 1;
 
-		this.headers = {};
-		this.request = new XMLHttpRequest();
-		this.request.onerror = this.onerror.bind(this);
+		this.retry = setTimeout(this.resend.bind(this), this.delay);
+	}
+}
 
-		return this;
+class AjaxRequest {
+	timeout = 5000
+	retries = 3
+	delay = 1000
+
+	constructor(url) {
+		this.url = url
+
+		this.headers = {}
+		this.request = new XMLHttpRequest()
+		this.request.onerror = errorHandler.bind(this)
 	}
 
-	AjaxRequest.prototype.timeout = 5000;
-	AjaxRequest.prototype.retries = 3;
-	AjaxRequest.prototype.delay = 1000;
+	get() {
+		this.method = 'GET'
 
-	AjaxRequest.prototype.onerror = function() {
-		if (this.retries === 0) {
-			this.failure();
-		}
-		else {
-			this.retries -= 1;
+		this.send()
+	}
 
-			this.retry = setTimeout(this.resend.bind(this), this.delay);
-		}
-	};
+	header(name, value) {
+		this.headers[name] = value
 
-	AjaxRequest.prototype.url = function(url) {
-	    this.url = url;
+		return this
+	}
 
-		return this;
-	};
+	success(callback) {
+		let request = this.request
 
-	AjaxRequest.prototype.header = function(name, value) {
-		this.headers[name] = value;
-
-		return this;
-	};
-
-	AjaxRequest.prototype.success = function(callback) {
-		var request = this.request;
-
-		request.onload = function() {
-			callback(request.status, request.responseText);
-		};
-
-		return this;
-	};
-
-	AjaxRequest.prototype.failure = function(callback) {
-		this.failure = callback;
-
-		return this;
-	};
-
-	AjaxRequest.prototype.send = function() {
-		this.request.open(this.method, this.url);
-
-		for (var name in this.headers) {
-			this.request.setRequestHeader(name, this.headers[name]);
+		request.onload = () => {
+			callback(request.status, request.responseText)
 		}
 
-		this.request.timeout = this.timeout;
-		this.request.ontimeout = this.onerror.bind(this);
+		return this
+	}
 
-		this.request.send();
+	failure(callback) {
+		this.failure = callback
 
-		return this;
-	};
+		return this
+	}
 
-	AjaxRequest.prototype.resend = function() {
-	    this.request.abort();
+	send() {
+		this.setupRequest()
 
-		this.send();
-	};
+		this.request.send()
+	}
 
-	AjaxRequest.prototype.get = function() {
-		this.method = 'GET';
+	setupRequest() {
+		this.request.open(this.method, this.url)
 
-		this.send();
-	};
+		this.attachHeaders()
 
-	AjaxRequest.prototype.abort = function() {
-	    this.request.abort();
+		this.request.timeout = this.timeout
+		this.request.ontimeout = this.request.onerror
+	}
 
+	attachHeaders() {
+		for (let name in this.headers) {
+			this.request.setRequestHeader(name, this.headers[name])
+		}
+	}
+
+	resend() {
+	    this.abort()
+
+		this.send()
+	}
+
+	abort() {
 		if (this.retry !== undefined) {
-			clearTimeout(this.retry);
+			clearTimeout(this.retry)
 		}
-	};
 
-	return function(url) {
-		return new AjaxRequest(url);
-	};
-})();
+		this.request.abort()
+	}
+}
+
+export function Ajax(url) {
+	return new AjaxRequest(url)
+}
