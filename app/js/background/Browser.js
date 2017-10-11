994b86f066628a2d391d90025fb553c6ce4629d8
ideoderek <ideoderek@gmail.com>
2017-10-11 08:24:55 -0230

    Refactors Browser to ES2015

--- app/js/background/Browser.js
+++ app/js/background/Browser.js
@@ -1,71 +1,32 @@
-var Browser = (function() {
-	var exports = {};
+let emptyCallback = () => {}
 
-	var emptyCallback = function(){};
+export let Browser = {
+	open: url => {
+		chrome.tabs.create({url: url})
+	},
 
-	exports.storage = {
-		get: function(key) {
-			var value;
-
-			try {
-				value = JSON.parse(localStorage.getItem(key));
-			}
-			catch (e) {
-				value = null;
-			}
-
-			return value;
-		},
-		all: function() {
-			var values = {},
-				length = localStorage.length;
-
-			for (var i = 0; i < length; i++) {
-				var key = localStorage.key(i);
-
-				values[key] = this.get(key);
-			}
-
-			return values;
-		},
-		set: function(key, value) {
-			localStorage.setItem(key, JSON.stringify(value));
-		},
-		remove: function(key) {
-			localStorage.removeItem(key);
-		},
-		clear: function() {
-			localStorage.clear();
-		}
-	};
-
-	exports.open = function(url) {
-		chrome.tabs.create({url: url});
-	};
-
-	exports.openOptions = function() {
+	openOptions: () => {
 		if (chrome.runtime.openOptionsPage) {
-			chrome.runtime.openOptionsPage();
-		} else {
-			this.open(chrome.runtime.getURL('options.html'));
+			chrome.runtime.openOptionsPage()
 		}
-	};
+		else {
+			this.open(chrome.runtime.getURL('options.html'))
+		}
+	},
 
-	exports.notification = function(id, options) {
+	notification: (id, options) => {
 		chrome.notifications.create(id, options, emptyCallback);
-	};
+	},
 
-	exports.clearNotification = function(id) {
-		chrome.notifications.clear(id, emptyCallback);
-	};
+	clearNotification: id => {
+		chrome.notifications.clear(id, emptyCallback)
+	},
 
-	exports.badge = function(text, color) {
+	badge: (text, color) => {
 		if (color !== undefined) {
-			chrome.browserAction.setBadgeBackgroundColor({ color: color });
+			chrome.browserAction.setBadgeBackgroundColor({ color: color })
 		}
 
-		chrome.browserAction.setBadgeText({ text: String(text) });
-	};
-
-	return exports;
-})();
+		chrome.browserAction.setBadgeText({ text: String(text) })
+	},
+}
