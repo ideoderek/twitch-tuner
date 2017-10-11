792fffe261eb03ce4fefc2182def2a2f2e8dcac9
ideoderek <ideoderek@gmail.com>
2017-10-11 14:05:17 -0230

    Converts the export object in Browser.js to a class

--- app/js/background/Browser.js
+++ app/js/background/Browser.js
@@ -1,34 +1,32 @@
-export {Browser as default}
-
 let emptyCallback = () => {}
 
-let Browser = {
-	open: function(url) {
+export default class Browser {
+	open(url) {
 		chrome.tabs.create({url: url})
-	},
+	}
 
-	openOptions: function() {
+	openOptions() {
 		if (chrome.runtime.openOptionsPage) {
 			chrome.runtime.openOptionsPage()
 		}
 		else {
 			this.open(chrome.runtime.getURL('options.html'))
 		}
-	},
+	}
 
-	notification: function(id, options) {
-		chrome.notifications.create(id, options, emptyCallback);
-	},
+	notification(id, options) {
+		chrome.notifications.create(id, options, emptyCallback)
+	}
 
-	clearNotification: function(id) {
+	clearNotification(id) {
 		chrome.notifications.clear(id, emptyCallback)
-	},
+	}
 
-	badge: function(text, color) {
+	badge(text, color) {
 		if (color !== undefined) {
 			chrome.browserAction.setBadgeBackgroundColor({ color: color })
 		}
 
 		chrome.browserAction.setBadgeText({ text: String(text) })
-	},
+	}
 }
