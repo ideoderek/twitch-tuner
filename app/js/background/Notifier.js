2768d6d36a14417b33213b4ba4109c911db73d01
ideoderek <ideoderek@gmail.com>
2017-10-11 08:25:15 -0230

    Refactors Notifier to ES2015

--- app/js/background/Notifier.js
+++ app/js/background/Notifier.js
@@ -1,216 +1,268 @@
-var Notifier = function(Storage, Browser) {
-	var q = {
-		check: function(name, then) {
-			if (Storage.get(name) === true) {
-				return true;
-			}
+const BADGE_COLOR = '#6441A5'
+const BADGE_ENABLED_KEY = 'Badge_Enabled'
 
-			return false;
-		},
-		get badgeEnabled() {
-			return this.check('Badge_Enabled');
-		},
-		get notificationsEnabled() {
-			return this.check('Notifications_Enabled');
-		},
-		get showGame() {
-			return this.check('Notifications_ShowGame');
-		},
-		get showViewers() {
-			return this.check('Notifications_ShowViewers');
-		},
-		get showDescription() {
-			return this.check('Notifications_ShowDescription');
-		},
-		get favoritesOnly() {
-			return this.check('Notifications_FavoritesOnly');
-		},
-	};
+const NOTIFICATIONS_ENABLED_KEY = 'Notifications_Enabled'
+const SHOW_GAME_KEY = 'Notifications_ShowGame'
+const SHOW_VIEWERS_KEY = 'Notifications_ShowViewers'
+const SHOW_DESCRIPTION_KEY = 'Notifications_ShowDescription'
+const FAVORITES_ONLY_KEY = 'Notifications_FavoritesOnly'
 
-	Storage.listen('Badge_Enabled', updateBadge)
+MULTI_CLICK_URL = 'https://twitch.tv/directory/following/live'
+SINGLE_CLICK_URL_PREFIX = 'https://twitch.tv/'
 
-	var liveChannels = [],
-		count = 0;
+var id = 0
 
-	var commonOptions = function() {
+class Notification {
+	constructor(data) {
+		this.data = data
+	}
+
+	options() {
 		return {
 			isClickable: true,
 			iconUrl: '../img/icon128.png',
-			title: '',
-			message: ''
-		};
-	};
-
-	var generateId = (function() {
-		var id = 0;
-
-		return function() {
-			return String(id++);
-		};
-	})();
-
-	function getTitle(stream) {
-		return stream.displayName + ' is live!';
+			type: this.type(),
+			title: this.title()
+		}
 	}
 
-	function getMessage(stream) {
-		var message = [];
+	spawn(browser) {
+		browser.notification(this.id(), this.options())
+	}
 
-		if (q.showGame && stream.game) {
-			message.push(stream.game);
+	config(key) {
+		return this.store.get(key) === true
+	}
+
+	get showGame() {
+		return this.config(SHOW_GAME_KEY)
+	}
+
+	get showViewers() {
+		return this.config(SHOW_VIEWERS_KEY)
+	}
+
+	get showDescription() {
+		return this.config(SHOW_DESCRIPTION_KEY)
+	}
+
+	static click(id) {
+		if (id.indexOf(this.idPrefix) === 0) {
+			this.browser.open(this.clickUrl(id))
+
+			this.browser.clearNotification(id)
+		}
+	}
+}
+
+class SingleNotification extends Notification {
+	idPrefix = 'single_'
+
+	id() {
+		return this.idPrefix + this.data.name
+	}
+
+	type() {
+		return 'basic'
+	}
+
+	title() {
+		return this.data.displayName + ' is live!';
+	}
+
+	options() {
+		let options = super.options()
+		let message = this.message()
+
+		if (message === null) {
+			options.message = this.contextMessage()
+		}
+		else {
+			options.message = message
+			options.contextMessage = this.contextMessage()
 		}
 
-		if (q.showViewers) {
-			var end = stream.formattedViewers === 1 ? 'viewer' : 'viewers';
+		return options
+	}
 
-			message.push('for ' + stream.formattedViewers + ' ' + end);
+	message() {
+		let message = []
+
+		if (this.showGame && this.data.game !== null) {
+			message.push(this.data.game)
 		}
+		if (this.showViewers) {
+			let suffix = this.data.viewers === 1 ? '' : 's'
 
+			message.push(`for ${this.data.formattedViewers} viewer${suffix}`)
+		}
 		if (message.length > 0) {
-			message.unshift('Streaming');
+			message.unshift('Playing')
 
-			return message.join(' ');
-		}
-
-		return '';
-	}
-
-	function getContextMessage(stream) {
-		if (q.showDescription) {
-			return stream.description;
-		}
-
-		return '';
-	}
-
-	function getItems(streamCount, streams) {
-		var items = [];
-
-		streamCount = streamCount < 5 ? streamCount : 5;
-
-		for (var i = 0; i < streamCount; i++) {
-			var item = {},
-				stream = streams[i];
-
-			item.title = stream.favorite ? '\u2661' : '>';
-			item.title += ' ' + stream.displayName;
-
-			if (q.showGame && stream.game) {
-				item.message = stream.game;
-			}
-			else if (q.showViewers) {
-				item.message = stream.formattedViewers + ' viewers';
-			}
-			else if (q.showDescription) {
-				item.message = stream.description;
-			}
-			else {
-				item.message = '';
-			}
-
-			items.push(item);
-		}
-
-		return items;
-	}
-
-	function createNotification(id, options) {
-		Browser.notification(id, options);
-	}
-
-	function singleNotification(stream) {
-		var options = commonOptions();
-		options.type = 'basic';
-		options.title = getTitle(stream);
-
-		var message = getMessage(stream);
-		if (message) {
-			options.message = message;
-			options.contextMessage = getContextMessage(stream);
-		}
-		else {
-			options.message = getContextMessage(stream);
-		}
-
-		createNotification(stream.name, options);
-	}
-
-	function multiNotification(streams) {
-		var streamCount = streams.length,
-			options = commonOptions();
-		options.type = 'list';
-		options.title = streamCount + ' channels are now live!';
-
-		if (streamCount > 5) {
-			options.contextMessage = 'and ' + (streamCount - 5) + ' more!';
-		}
-
-		options.items = getItems(streamCount, streams);
-
-		createNotification('multi_' + generateId(), options);
-	}
-
-	function updateBadge() {
-		if (q.badgeEnabled) {
-			Browser.badge(count, '#6441A5');
-		}
-
-		else {
-			// This will effectively disable the badge
-			Browser.badge('');
+			return message.join(' ')
 		}
 	}
 
-	function createNotifications(streams) {
-		if (! q.notificationsEnabled) {
-			return;
+	contextMessage() {
+		return this.showDescription ? this.data.description : ''
+	}
+
+	clickUrl(id) {
+		return SINGLE_CLICK_URL_PREFIX + id
+	}
+}
+
+class MultiNotification extends Notification {
+	idPrefix = 'multi_'
+
+	id() {
+		return this.idPrefix + String(id++)
+	}
+
+	type() {
+		return 'list'
+	}
+
+	title() {
+		return `${this.count()} channels just went live!`
+	}
+
+	options() {
+		let options = super.options()
+
+		options.items = this.items()
+		options.contextMessage = this.contextMessage()
+
+		return options
+	}
+
+	items() {
+		let items = []
+		let count = this.count()
+
+		for (let i = 0; i < count && i < 5; i++) {
+			items.push(this.item(this.data[i]))
+		}
+
+		return items
+	}
+
+	item(stream) {
+		item.title = stream.favorite ? '\u2661' : '>'
+
+		return {
+			title: this.itemTitle(stream),
+			message: this.itemMessage(stream)
+		}
+	}
+
+	itemTitle(stream) {
+		let bullet = stream.favorite ? '\u2661' : '>'
+
+		return bullet + ' ' + stream.displayName
+	}
+
+	itemMessage(stream) {
+		if (this.showGame) {
+			return stream.game || ''
+		}
+		else if (this.showViewers) {
+			return `${stream.formattedViewers} ${stream.viewers === 1 ? 'viewer' : 'viewers'}`
+		}
+		else if (this.showDescription) {
+			return `'${stream.description}'`
+		}
+
+		return ''
+	}
+
+	contextMessage() {
+		let extra = this.count() - 5
+
+		if (extra > 0) {
+			return `and ${extra} more!`
+		}
+
+		return ''
+	}
+
+	count() {
+		return this.data.length
+	}
+
+	clickUrl() {
+		return MULTI_CLICK_URL
+	}
+}
+
+export class Notifier {
+	constructor(Storage, Browser) {
+		this.store = Storage
+		this.browser = Browser
+
+		this.streamCount = 0
+		this.liveChannels = []
+
+		this.badgeEnabled = this.store.get(BADGE_ENABLED_KEY)
+		this.store.listen(BADGE_ENABLED_KEY, this.toggleBadge.bind(this))
+
+		chrome.notifications.onClicked.addListener(this.onNotificationClick)
+	}
+
+	update(channels) {
+		this.streamCount = channels.countStreams()
+		this.updateBadge()
+
+		let alertable = channels.filterStreams(this.isAlertable)
+		this.notify(alertable)
+
+		this.liveChannels = channels.getLiveChannelNames()
+	}
+
+	notify(streams) {
+		if (this.store.get(NOTIFICATIONS_ENABLED_KEY) === false) {
+			return
 		}
 
 		if (streams.length > 3) {
-			multiNotification(streams);
+			new MultiNotification(streams, this.browser, this.store)
 		}
 		else {
-			streams.forEach(singleNotification);
+			streams.forEach((stream) => {
+				new SingleNotification(stream, this.browser, this.store)
+			})
 		}
 	}
 
-	function clearNotification(id) {
-		Browser.clearNotification(id);
-	}
+	toggleBadge(on) {
+		this.badgeEnabled = on
 
-	function onNotificationClick(id) {
-		if (id.indexOf('multi_') >= 0) {
-			Browser.open('https://twitch.tv/directory/following/live');
+		if (on) {
+			this.updateBadge()
 		}
 		else {
-			Browser.open('https://twitch.tv/' + id);
+			this.browser.badge('')
 		}
-
-		clearNotification(id);
 	}
 
-	chrome.notifications.onClicked.addListener(onNotificationClick);
-
-	function isAlertable(stream) {
-		if (q.favoritesOnly && ! stream.favorite) {
-			return false;
+	updateBadge() {
+		if (this.badgeEnabled) {
+			this.browser.badge(this.streamCount, BADGE_COLOR)
 		}
-
-		if (liveChannels.indexOf(stream.name) === -1) {
-			return true;
-		}
-
-		return false;
 	}
 
-	return function(channels) {
-		count = channels.countStreams();
+	onNotificationClick(id) {
+		SingleNotification.click(id)
+		MultiNotification.click(id)
+	}
 
-		updateBadge();
-		createNotifications(channels.filterStreams(isAlertable));
+	isAlertable(stream) {
+		let favoritesOnly = this.store.get(FAVORITES_ONLY_KEY) === true
 
-		// This has to happen after the call to createNotifications
-		// or else isAlertable will always return false
-		liveChannels = channels.getLiveChannelNames();
-	};
-}(Storage, Browser);
+		if (favoritesOnly && ! stream.favorite) {
+			return false
+		}
+
+		return liveChannels.indexOf(stream.name) === -1
+	}
+}
