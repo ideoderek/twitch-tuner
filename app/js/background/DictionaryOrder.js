bdf3edce0a64098f3d0375a4ddddb5d0a71b4e7d
ideoderek <ideoderek@gmail.com>
2017-10-11 08:24:14 -0230

    Refactors DictionaryOrder and DictionarySorter to ES2015

--- app/js/background/DictionaryOrder.js
+++ app/js/background/DictionaryOrder.js
@@ -1,75 +1,76 @@
-function DictionaryOrder(dictionary, descriptor) {
-    this.sorter = new DictionarySorter(descriptor);
-    this.reset(dictionary);
+import {DictionarySorter as Sorter} from "DictionarySorter.js"
+import Set from "Set.js"
+
+export class DictionaryOrder {
+	constructor(dictionary, descriptor) {
+		this.sorter = new Sorter(descriptor)
+
+		this.reset(dictionary)
+	}
+
+	reset(dictionary) {
+		this.dictionary = dictionary
+
+	    this.order = new Set()
+	    this.index = 0
+
+	    this.sorter.define(this.dictionary, this.order)
+	}
+
+	keys() {
+		return this.order.slice()
+	}
+
+	count() {
+		return this.order.length
+	}
+
+	add(key) {
+		this.order.add(key)
+	}
+
+	remove(key) {
+	    this.order.remove(key)
+	}
+
+	sort() {
+	    if (this.count() > 1) {
+	        this.sorter.sort()
+	    }
+	}
+
+	configureSorting(descriptor) {
+	    this.sorter.configure(descriptor)
+
+	    this.sort()
+	}
+
+	update(key) {
+	    this.sorter.update(key)
+	}
+
+	reduce(reducer, value) {
+		let keys = this.order
+
+	    for (let i in keys) {
+	        value = reducer(this.dictionary[keys[i]], value)
+	    }
+
+	    return value
+	}
+
+	filter(filter) {
+		let keys = this.order
+		let result = []
+
+	    for (let i in keys) {
+	        let element = this.dictionary[keys[i]]
+
+	        if (filter(element)) {
+	            result.push(element)
+	        }
+	    }
+
+	    return result
+	}
 }
-
-DictionaryOrder.prototype.reset = function(dictionary) {
-    this.dictionary = dictionary;
-
-    this.order = [];
-    this.index = 0;
-
-    this.sorter.define(this.dictionary, this.order);
-};
-
-DictionaryOrder.prototype.keys = function() {
-    return this.order.slice();
-};
-
-DictionaryOrder.prototype.count = function() {
-    return this.order.length;
-};
-
-DictionaryOrder.prototype.add = function(key) {
-    if (hasElement(this.order, key)) {
-        return;
-    }
-
-    addToSet(this.order, key);
-};
-
-DictionaryOrder.prototype.update = function(key) {
-    this.sorter.update(key);
-};
-
-DictionaryOrder.prototype.remove = function(key) {
-    removeFromSet(this.order, key);
-};
-
-DictionaryOrder.prototype.sort = function() {
-    if (this.count() > 1) {
-        this.sorter.sort();
-    }
-};
-
-DictionaryOrder.prototype.configureSorting = function(descriptor) {
-    this.sorter.configure(descriptor);
-
-    this.sort();
-};
-
-DictionaryOrder.prototype.reduce = function(reducer, value) {
-    var keys = this.order.slice();
-
-    for (var i in keys) {
-        value = reducer(this.dictionary[keys[i]], value);
-    }
-
-    return value;
-};
-
-DictionaryOrder.prototype.filter = function(filter) {
-    var keys = this.order,
-        data = this.dictionary,
-        result = [];
-
-    for (var i in keys) {
-        var element = data[keys[i]];
-
-        if (filter(element)) {
-            result.push(element);
-        }
-    }
-
-    return result;
-};
--- app/js/background/DictionarySorter.js
+++ app/js/background/DictionarySorter.js
@@ -1,95 +1,85 @@
-function DictionarySorter(descriptor) {
-	this.configure(descriptor);
+function compare() {
+	var aAttr = a[this.groupBy],
+		bAttr = b[this.groupBy],
+		result;
+
+	if (aAttr !== bAttr) {
+		result = aAttr > bAttr ? 1 : -1;
+		result = typeof aAttr === 'string' ? -result : result;
+		return this.groupDirection ? -result : result;
+	}
+
+	aAttr = a[this.sortBy];
+	bAttr = b[this.sortBy];
+
+	if (aAttr !== bAttr) {
+		result = aAttr > bAttr ? 1 : -1;
+		result = typeof aAttr === 'string' ? -result : result;
+		return this.sortDirection ? -result : result;
+	}
+
+	return 0;
 }
 
-DictionarySorter.prototype.define = function(dictionary, order) {
-	this.dictionary = dictionary;
-	this.order = order;
-};
-
-DictionarySorter.prototype.configure = function(descriptor) {
-	this.descriptor = this.descriptor === undefined ? {} : this.descriptor;
-
-	for (var key in descriptor) {
-		this.descriptor[key] = descriptor[key];
-	}
-
-	this.generateSorter(
-		this.descriptor.GroupAttribute,
-		this.descriptor.GroupDescending,
-		this.descriptor.SortAttribute,
-		this.descriptor.SortDescending
-	);
-};
-
-DictionarySorter.prototype.generateSorter = function(groupAttribute, groupDescending, sortAttribute, sortDescending) {
-	this.compare = function(a, b) {
-		var aAttr = a[groupAttribute],
-			bAttr = b[groupAttribute],
-			result;
-
-		if (aAttr !== bAttr) {
-			result = aAttr > bAttr ? 1 : -1;
-			result = typeof aAttr === 'string' ? -result : result;
-			return groupDescending ? -result : result;
+export class DictionarySorter {
+	constructor(descriptor) {
+		for (let key in descriptor) {
+			this[key] = descriptor[key]
 		}
 
-		aAttr = a[sortAttribute];
-		bAttr = b[sortAttribute];
-
-		if (aAttr !== bAttr) {
-			result = aAttr > bAttr ? 1 : -1;
-			result = typeof aAttr === 'string' ? -result : result;
-			return sortDescending ? -result : result;
-		}
-
-		return 0;
-	};
-};
-
-/* This could be sped up significantly, but is not called often. */
-DictionarySorter.prototype.update = function(key) {
-	var data = this.dictionary,
-		keys = this.order,
-		index = keys.indexOf(key);
-
-	if (index === -1) {
-		return;
+		this.compare = compare.bind(this)
 	}
 
-	keys.splice(index, 1);
+	define(dictionary, order) {
+		this.dictionary = dictionary
 
-	for (var i in keys) {
-		if (this.compare(data[key], data[keys[i]]) <= 0) {
-			keys.splice(i, 0, key);
-
-			return;
-		}
+		this.order = order
 	}
 
-	keys.splice(keys.length, 0, key);
-};
+	// TODO: speed this up
+	update(key) {
+		let data = this.dictionary
+		let keys = this.order
+		let index = keys.indexOf(key)
 
-/*	This method uses a (probably poorly-implemented) insertion sort.
-	Insertion sort runs especially quickly on nearly sorted data.
-	Making it well-suited to sorting channels/streams after the first update.
-	Heap sort would probably be a better solution for the initial update. */
-DictionarySorter.prototype.sort = function() {
-	var data = this.dictionary,
-		keys = this.order,
-		length = keys.length;
-
-	for (var i = 1; i < length; i++) {
-		var key = keys[i],
-			j = i;
-
-		while (j > 0 && this.compare(data[key], data[keys[j - 1]]) < 0) {
-			j -= 1;
+		if (index === -1) {
+			return
 		}
 
-		keys.splice(i, 1);
-		keys.splice(j, 0, key);
+		keys.splice(index, 1)
+
+		for (let i in keys) {
+			if (this.compare(data[key], data[keys[i]]) <= 0) {
+				keys.splice(i, 0, key);
+
+				return;
+			}
+		}
+
+		keys.splice(keys.length, 0, key);
 	}
 
-	return keys;
-};
+	// TODO: This method uses insertion sort
+	// Insertion sort is well-suited to nearly sorted data
+	// Which makes it appropriate for non-initial updates
+	// A different algorithm would be better for the initial update
+	sort() {
+		let data = this.dictionary
+		let keys = this.order
+		let length = keys.length
+
+		for (let i = 1; i < length; i++) {
+			let key = keys[i]
+			let j = i
+
+			while (j > 0 && this.compare(data[key], data[keys[j - 1]]) < 0) {
+				j -= 1
+			}
+
+			keys.splice(i, 1)
+			keys.splice(j, 0, key)
+		}
+
+		return keys
+	}
+}
