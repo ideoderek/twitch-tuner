21488621510eaf751eb6c7a5246b5015b9f19c1b
ideoderek <ideoderek@gmail.com>
2017-10-15 16:08:16 -0230

    Reorganizes and fixes bugs in DictionarySorter

--- app/js/background/DictionarySorter.js
+++ app/js/background/DictionarySorter.js
@@ -1,31 +1,6 @@
-function compare() {
-	var aAttr = a[this.groupBy],
-		bAttr = b[this.groupBy],
-		result;
-
-	if (aAttr !== bAttr) {
-		result = aAttr > bAttr ? 1 : -1;
-		result = typeof aAttr === 'string' ? -result : result;
-		return this.groupDirection ? -result : result;
-	}
-
-	aAttr = a[this.sortBy];
-	bAttr = b[this.sortBy];
-
-	if (aAttr !== bAttr) {
-		result = aAttr > bAttr ? 1 : -1;
-		result = typeof aAttr === 'string' ? -result : result;
-		return this.sortDirection ? -result : result;
-	}
-
-	return 0;
-}
-
 export class DictionarySorter {
 	constructor(descriptor) {
 		this.configure(descriptor)
-
-		this.compare = compare.bind(this)
 	}
 
 	configure(descriptor) {
@@ -40,39 +15,35 @@ export class DictionarySorter {
 		this.order = order
 	}
 
-	// TODO: speed this up
 	update(key) {
-		let data = this.dictionary
-		let keys = this.order
-		let index = keys.indexOf(key)
-
-		if (index === -1) {
+		if (! this.order.has(key)) {
 			return
 		}
 
-		keys.splice(index, 1)
+		this.order.remove(key)
+
+		let value = this.dictionary[key]
+		let keys = this.order.all()
 
 		for (let i in keys) {
-			if (this.compare(data[key], data[keys[i]]) <= 0) {
-				keys.splice(i, 0, key);
+			let current = this.dictionary[keys[i]]
 
-				return;
+			if (this.compare(value, current) <= 0) {
+				this.order.insertBefore(key, i)
+
+				return
 			}
 		}
 
-		keys.splice(keys.length, 0, key);
+		this.order.add(key)
 	}
 
-	// TODO: This method uses insertion sort
-	// Insertion sort is well-suited to nearly sorted data
-	// Which makes it appropriate for non-initial updates
-	// A different algorithm would be better for the initial update
 	sort() {
 		let data = this.dictionary
-		let keys = this.order
-		let length = keys.length
+		let length = this.order.length
 
 		for (let i = 1; i < length; i++) {
+			let keys = this.order.all()
 			let key = keys[i]
 			let j = i
 
@@ -80,10 +51,31 @@ export class DictionarySorter {
 				j -= 1
 			}
 
-			keys.splice(i, 1)
-			keys.splice(j, 0, key)
+			this.order.remove(key)
+			this.order.insertBefore(key, j)
+		}
+	}
+
+	compare(a, b) {
+		let aAttr = a[this.groupBy]
+		let bAttr = b[this.groupBy]
+		let result
+
+		if (aAttr !== bAttr) {
+			result = aAttr > bAttr ? 1 : -1
+			result = typeof aAttr === 'string' ? -result : result
+			return this.groupDirection ? -result : result
 		}
 
-		return keys
+		aAttr = a[this.sortBy]
+		bAttr = b[this.sortBy]
+
+		if (aAttr !== bAttr) {
+			result = aAttr > bAttr ? 1 : -1
+			result = typeof aAttr === 'string' ? -result : result
+			return this.sortDirection ? -result : result
+		}
+
+		return 0
 	}
 }
