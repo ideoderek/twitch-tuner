270d20833486552794b3d63a5f09b93ec50b8b19
ideoderek <ideoderek@gmail.com>
2017-10-25 07:15:50 -0230

    Adds ModeToggle module file for dark mode toggle, & adds to popup script

--- app/js/popup/main.js
+++ app/js/popup/main.js
@@ -1,5 +1,6 @@
 import ChannelList from "./ChannelList.js"
 import StreamList from "./StreamList.js"
+import ModeToggle from "./ModeToggle.js"
 import UserBar from "./UserBar.js"
 import TabBar from "./TabBar.js"
 import Timer from "./Timer.js"
@@ -8,7 +9,9 @@ const DEFAULT_TAB = 'streams'
 
 let app = chrome.extension.getBackgroundPage().TwitchTuner
 let channels = app.getChannelContainer()
+let storage = app.getStorage()
 
+let modeToggler = new ModeToggle(storage)
 let userBar = new UserBar(setUsername)
 let timer = new Timer(reload)
 let channelList = new ChannelList(channels, app, getData)
